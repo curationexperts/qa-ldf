f447d7e09ddbcb47b892f41ff38f8deb20e594a2
Tom Johnson <tom@dp.la>
2017-01-15 15:06:41 -0800

    Add version, initial docs, and a first spec

--- lib/qa/ldf.rb
+++ lib/qa/ldf.rb
@@ -1,4 +1,14 @@
+require 'qa/ldf/version'
+##
+# @see https://github.com/projecthydra-labs/questioning_authority Qa
 module Qa
+  ##
+  # Providies bindings from `qa` to a linked data fragment caching service for 
+  # fast query of RDF-based authorities.
+  #
+  # @see https://github.com/projecthydra-labs/questioning_authority Qa
+  # @see https://github.com/ActiveTriples/linked-data-fragments Linked Data 
+  #   Fragments Cache
   module LDF
   end
 end
--- spec/qa/ldf_spec.rb
+++ spec/qa/ldf_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Qa::LDF do
-  it 'works'
+  it 'has a version' do
+    expect(described_class::VERSION.to_str).to be_a String
+  end
 end
