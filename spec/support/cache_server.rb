b1739b6ef476eee8b3a90414f2b6b8b7db7fe755
Tom Johnson <tom@dp.la>
2017-02-05 12:40:23 -0800

    Finish setup/teardown of server in shared context

--- spec/support/cache_server.rb
+++ spec/support/cache_server.rb
@@ -4,9 +4,22 @@ require 'linked_data_fragments/cache_server'
 require 'linked_data_fragments/repository'
 
 shared_context 'with cache server' do
+  let(:server_endpoint) { 'http://ldcache.example.com/' }
+
   before(:context) do
+    server_endpoint = 'ldcache.example.com'
+
     ShamRack
-      .at('ldcache.example.com')
+      .at(server_endpoint)
       .mount(LinkedDataFragments::CacheServer::APPLICATION)
+
+    Qa::LDF.configure! do |config|
+      config[:endpoint] = "http://#{server_endpoint}"
+    end
+  end
+
+  after(:context) do
+    ShamRack.reset
+    Qa::LDF::Configuration.instance.reset!
   end
 end
